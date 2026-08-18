a0f35c71e7173d6012c0973d51a4a02f90474dad
cheerystal-stack <cheerystal@gmail.com>
2026-08-18 23:22:39 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -15,6 +15,18 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
